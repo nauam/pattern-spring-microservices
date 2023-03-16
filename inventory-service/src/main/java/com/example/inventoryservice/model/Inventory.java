959dc0230f9d7453056c0935ab4a679b44b52f6d
nauam <nauamvictor@outlook.com>
2023-03-16 12:12:10 -0300

    feat(inventory-service): Implementaçao do inventory-service, incluido o test automatizado com JUnit5 e testcontainers

---
+++ inventory-service/src/main/java/com/example/inventoryservice/model/Inventory.java
@@ -0,0 +1,24 @@
+package com.example.inventoryservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="t_inventory")
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+}
